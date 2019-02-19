6be80f7fba202cf0ac10cd91ac916e084e36a146
ivan kulibin <rudewalt@gmail.com>
2019-02-19 23:02:40 +0300

    feat(shopingCartService) - обновил контракт

--- api/src/Sibintek.BeerMachine/Services/ShoppingCartService.cs
+++ api/src/Sibintek.BeerMachine/Services/ShoppingCartService.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using Sibintek.BeerMachine.Settings;
 using Sibintek.BeerMachine.Domain;
 
@@ -33,32 +32,24 @@ namespace Sibintek.BeerMachine.Services
 
         private static ShoppingCart Parse(string responseText)
         {
-            const short responseArrayLength = 4;
+            var response = JsonConvert.DeserializeObject<ShoppingCartResponse>(responseText);
 
-            var jsonArray = JArray.Parse(responseText);
-            if (jsonArray.Count < responseArrayLength)
-            {
-                throw new Exception("wrong response from shopping cart service");
-            }
-
-            var productNames = jsonArray[0].ToObject<string[]>();
-            var productCounts = jsonArray[1].ToObject<int[]>();
-            var productPrices = jsonArray[2].ToObject<decimal[]>();
-            // Лучше сами посчитаем
-            // var total = jsonArray[3].ToObject<decimal>();
-
-            if (productNames.Length != productCounts.Length || productNames.Length != productPrices.Length)
-            {
-                throw new Exception("wrong array length in response from shopping cart service");
-            }
-
-            var items = productNames
-                .Zip(productCounts, (name, count) => (name, count))
-                .Zip(productPrices,
+            var items = response.products
+                .Zip(response.quantity, (name, count) => (name, count))
+                .Zip(response.price_for_each,
                     ((string name, int count) x, decimal price) => new ShoppingCart.Item(x.name, price, x.count))
                 .ToList();
 
             return new ShoppingCart(items);
+        }
+        
+        private class ShoppingCartResponse
+        {
+            public decimal[] price_for_each { get; set; }
+            
+            public string[] products { get; set; }
+            
+            public int[] quantity { get; set; }
         }
     }
 }
